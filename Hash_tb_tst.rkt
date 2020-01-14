3c3e92df19c769beda6c284b912905c5c41135b0
Tin <lord66tin66@gmail.com>
2020-01-14 17:39:24 +0100

    hash-tb start samo

---
+++ Hash_tb_tst.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+
+
+(define comm-hash-tb #hash(('STP . prog-row-halt)
+                           ('STO . prog-rndm-halt)
+                           ('RUP . prog-dir-UP)
+                           ()))
